62867ece21a6da6aa32900bc10261a1dceb338fc
Gurpreet Singh Saini <geezsaini@yahoo.com>
2012-11-01 18:07:04 -0700

    Top down analysis of sample poems

--- examples/jackbuilt.lisp
+++ examples/jackbuilt.lisp
@@ -1,5 +1,71 @@
 ;;;; Jackbuilt ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;
+;This is the house that Jack built. (POEM-OPENING-LINE)
+
+;This is the rat                    (VERSE-SUBJECT)
+;That ate the malt ; eating-line    (VERSE-DEPENDENT)
+;That lay in the house that Jack built. (VERSE-CLOSING)
+;
+;This is the cat,                   (VERSE-SUBJECT)
+;That killed the rat,               VERSE-DEPENDENT
+;That ate the malt                  VERSE-DEPDENCY-CHAIN, where chain = malt
+;That lay in the house that Jack built. VERSE-CLOSING
+;
+;This is the dog,                   VERSE-SUBJECT
+;That worried the cat,              VERSE-DEPENDENT
+;That killed the rat,               VERSE-DEPENDENCY-CHAIN, where chain = rat,malt
+;That ate the malt
+;That lay in the house that Jack built. VERSE-CLOSING
+;
+;This is the cow with the crumpled horn, VERSE-SUBJECT
+;That tossed the dog,                    VERSE-DEPENDENT
+;That worried the cat,                   VERSE-DEPENDENCY-CHAIN, where chain = cat,rat,malt 
+;That killed the rat,
+;That ate the malt
+;That lay in the house that Jack built.  VERSE-CLOSING
+;
+;This is the maiden all forlorn,             VERSE-SUBJECT
+;That milked the cow with the crumpled horn, VERSE-DEPENDENT
+;That tossed the dog,                        VERSE-DEPENDENCY-CHAIN, where chain = dog,cat,rat,malt
+;That worried the cat, 
+;That killed the rat,
+;That ate the malt
+;That lay in the house that Jack built.  VERSE-CLOSING
+;
+;This is the man all tattered and torn,
+;That kissed the maiden all forlorn,
+;That milked the cow with the crumpled horn,
+;That tossed the dog,
+;That worried the cat, 
+;That killed the rat,
+;That ate the malt
+;That lay in the house that Jack built. 
+
+;This is the priest all shaven and shorn,
+;That married the man all tattered and torn,
+;That kissed the maiden all forlorn,
+;That milked the cow with the crumpled horn,
+;That tossed the dog,
+;That worried the cat, 
+;That killed the rat,
+;That ate the malt 
+;That lay in the house that Jack built.
+
+;This is the cock that crowed in the morn,
+;That waked the priest all shaven and shorn,
+;That married the man all tattered and torn,
+;That kissed the maiden all forlorn,
+;That milked the cow with the crumpled horn,
+;That tossed the dog,
+;That worried the cat, 
+;That killed the rat,
+;That ate the malt
+;That lay in the house that Jack built. 
+
+
+
+
 
 (defvar jackbuilt-repeating-lines)
 (defvar jackbuilt-first-lines)
