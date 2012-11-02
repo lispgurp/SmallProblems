eff1a24985a4a99938c2cfd2b1a72b1214372474
Gurpreet Singh Saini <geezsaini@yahoo.com>
2012-11-02 12:45:48 -0700

    A new metlinguistic layer is emerging
    Apparently nothing in this world is a trivial as it looks. This is the
    second attempt to fully write this program and I am learning new things
    about the tiny domain I am trying to express directly

--- examples/coding.lisp
+++ examples/coding.lisp
@@ -3,32 +3,43 @@
 ;My TA just smiled.
 
 ;My program did nothing
-;So I started typing.
-;I added System.out.println("I <3 coding"),
-;I don't know why it wouldn't compile,
+;So I started typing.                             (VERSE-SUBJECT love)
+;I added System.out.println("I <3 coding"),       (REPETITION-CHAIN love)
+;I don't know why it wouldn't compile,            (VERSE-CLOSING-LINES)
 ;My TA just smiled.
 
 ;"Parse error," cried the compiler
-;Luckily I'm such a code baller.
-;I added a backslash to escape the quotes,
+;Luckily I'm such a code baller.                 (VERSE-SUBJECT error)
+;I added a backslash to escape the quotes,       (REPETITION-CHAIN error love)
 ;I added System.out.println("I <3 coding"),
-;I don't know why it wouldn't compile,
+;I don't know why it wouldn't compile,           (VERSE-CLOSING-LINES)
 ;My TA just smiled.
 
 ;Now the compiler wanted an identifier
-;And I thought the situation was getting dire.
-;I added a main method with its String[] args,
+;And I thought the situation was getting dire.   (VERSE-SUBJECT identifier)
+;I added a main method with its String[] args,   (REPETITION-CHAIN identifier backslash love)  
 ;I added a backslash to escape the quotes,
-;I added System.out.println("I <3 coding"),
+;I added System.out.println("I <3 coding"),     (VERSE-CLOSING-LINES)
 ;I don't know why it wouldn't compile,
 ;My TA just smiled.
 
 ;Java complained it expected an enum
-;Boy, these computers really are dumb!
-;I added a public class and called it Scum,
+;Boy, these computers really are dumb!           (VERSE-SUBJECT dumb)
+;I added a public class and called it Scum,      (REPETITION-CHAIN dumb identifier backslash love)
 ;I added a main method with its String[] args,
 ;I added a backslash to escape the quotes,
 ;I added System.out.println("I <3 coding"),
-;I don't know why it wouldn't compile,
+;I don't know why it wouldn't compile,          (VERSE-CLOSING-LINES)
 ;My TA just smiled.
 ;<< your custom 6th verse goes here >>
+
+(setf repetition-chain (scum main backslash love))
+
+'(dumb
+  :as-verse-subject "Java complained it expected an enum ~% Boy, these computers really are ~A ~%"
+  :as-in-repetition-chain "I added a public class and called it Scum")
+
+'(identifier
+  :as-verse-subject "Now the compiler wanted an ~A ~% And I thought the situation was getting dire ~%"
+  :as-in-repetition-chain "I added a main method with its String[] args")
+
--- examples/fly.lisp
+++ examples/fly.lisp
@@ -3,6 +3,11 @@
 ; 2. how those relationships are expressed in the poem in language
 ; 3. how the repetition expresses itself in each of the poems
 ; 4. the subject of the first verse, becomes the beginning of the chain of the second verse
+; 5. (restates #4 above) we introduce new link on the chain in each verse, like pusing on a stack 
+; 6. each object has a value in the poem as a subject, and a value in the poem as a repetition
+; 7. The essential nature of each poem is that there is a subject that finds themselves in
+;    a degenerate situation that deteriorates as each new object enters the situation 
+;    till it either ends (fly: she dies) or keeps going on forever (jack built/coding)
 
 ;There was an old woman who swallowed a fly.     (POEM-OPENING-LINE)
 ;I don't know why she swallowed that fly,        (VERSE-CLOSING-LINES)
@@ -10,20 +15,20 @@
  
 ;There was an old woman who swallowed a spider,   
 ;That wriggled and iggled and jiggled inside her. (VERSE-SUBJECT spider)
-;She swallowed the spider to catch the fly,       (VERSE-REPETITION-CHAIN fly)
+;She swallowed the spider to catch the fly,       (VERSE-REPETITION-CHAIN spider)
 ;I don't know why she swallowed that fly,         (VERSE-CLOSING-LINES)
 ;Perhaps she'll die.                              
  
 ;There was an old woman who swallowed a bird,    
 ;How absurd to swallow a bird.                   (VERSE-SUBJECT bird)
-;She swallowed the bird to catch the spider,     (VERSE-REPETITION-CHAIN spider fly)
+;She swallowed the bird to catch the spider,     (VERSE-REPETITION-CHAIN bird spider)
 ;She swallowed the spider to catch the fly,      
 ;I don't know why she swallowed that fly,        (VERSE-CLOSING-LINES)
 ;Perhaps she'll die.
  
 ;There was an old woman who swallowed a cat,    
 ;Imagine that to swallow a cat.                  (VERSE-SUBJECT)   (cat)
-;She swallowed the cat to catch the bird,        (VERSE-REPETITION-CHAIN bird spider fly)
+;She swallowed the cat to catch the bird,        (VERSE-REPETITION-CHAIN cat bird spider)
 ;She swallowed the bird to catch the spider,     
 ;She swallowed the spider to catch the fly,
 ;I don't know why she swallowed that fly,       (VERSE-CLOSING-LINES)
@@ -31,7 +36,7 @@
  
 ;There was an old woman who swallowed a dog,
 ;What a hog to swallow a dog.                   (VERSE-SUBJECT dog)
-;She swallowed the dog to catch the cat,        (VERSE-DEPENDENT cat bird spider fly)
+;She swallowed the dog to catch the cat,        (VERSE-REPETITION-CHAIN dog cat bird spider)
 ;She swallowed the cat to catch the bird,       
 ;She swallowed the bird to catch the spider, 
 ;She swallowed the spider to catch the fly,
@@ -43,17 +48,14 @@
 ;There was an old woman who swallowed a horse,  (POEM-ENDING-LINES)
 ;She died of course.
 
-'(dependency-chain 
-  ((dog cat)
-   (cat bird)
-   (bird spider)
-   (spider fly)))
+(setf repetition-chain  '(dog cat bird spider))
+  ;((dog cat)
+  ; (cat bird)
+  ; (bird spider)
+  ; (spider fly)))
 
 '(dog 
-  :opening "There was an old woman who swallowed a ~A ~% What a hog to swallow a ~A"
-  :dependent cat)
-
-(defun make-dependency-line-chain (subject dependent)
-  (format t "She swallowed a ~A to catch the ~A ~%" subject dependent))
+  :as-verse-subject        "There was an old woman who swallowed a ~A ~% What a hog to swallow a ~A"
+  :as-in-repetition-chain  "She swallowed the ~A to catch the cat")
 
 
