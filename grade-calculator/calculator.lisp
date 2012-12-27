f6849e9b53a0fc8c77e901883560de8297568de2
Gurpreet Saini <geezsaini@yahoo.com>
2012-12-26 19:49:01 -0800

    data objects defined
    just need to fill in the generic slots named field-name, and then we
    will be ready to define da algoritmz

--- grade-calculator/calculator.lisp
+++ grade-calculator/calculator.lisp
@@ -1,58 +1,144 @@
-; *Overview
+;c: 1.75
+; *overview*
 ;
-; This is part 2 in a continuing series to convince myself that little bitty
+; this is part 2 in a continuing series to convince myself that little bitty
 ; problems are fun and easily doable in a Real Language like Lisp (tm) as opposed
 ; to my blubstream programming. 
 ;
 
-; *Problem Statement
+; *problem statement*
 ;
-; This is one of the introductory problems in Computer Science and Engineering 142 at 
+; this is one of the introductory problems in Computer Science and Engineering 142 at 
 ; UW. In it the student has to create a command line driven application that, line by
 ; line defines thier grades for the quarter. See spec.pdf for more details
 ;
 
-; *Motivation 
+; *motivation* 
 ;
-; Basically, I have always wanted to use CL's condition system to decouple invalid input from main
+; basically, I have always wanted to use CL's condition system to decouple invalid input from main
 ; processing code. In the past I have tried to use Exceptions to do this, but the problem with
 ; this it's classic incarnation is that one loses to stack and cannot go back to where the error is 
 ; to do things differently. 
 ;
 
-; *** Data Structures for the Raw Data in the Grades Specification PDF *** ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- 
+; *** data objects ***********************************************************************************************
+; exam, homework, result
+
 (defclass exam ()
-  ((weight
+  (
+   (weight
     :accessor weight
     :initform (make-instance 'field 
-                              :display-prefix "What is its weight"
-                              :constraint (list :range 0 10)))))
-                             
-    
+                              :display-prefix "what is its weight?"
+                              :constraint '(:range 0 100)))
+    (score
+     :accessor score
+     :initform (make-instance 'field
+                              :display-prefix "score earned?"
+                              :constraint '(:range 0 100)))
+    (curve?
+     :accessor curve?
+     :initform (make-instance 'field
+                              :display-prefix "was there a curve?"
+                              :constraint '(:constants "Y" "N")))
+    (points
+     :documentation "calculated internally"
+     :accessor points
+     :initform (make-instance 'field
+                              :display-prefix "total points ="
+                              :constraint '(:range 0 100)))
+    (weighed-score
+     :documentation "calculated internally"
+     :accessor weighted-score
+     :initform (make-instance 'field
+                              :display-prefix "weighted Score ="
+                              :constraint '(:range 0 50)))
+   ) ; end slot defs
+) ; end classdef
 
-;  (score
-;   :accessor score)
-;  (curve?
-;   :accessor curve?)
-;  (total-points
-;   :accessor total-points)
-;  (weighted-score
-;   :accessor weighted-score))
+(defclass homework ()
+    (
 
-; TODO how to declare static objects aka singletons from Beverly Hills
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+     ) ; end slot defs
+); end defclass
+
+(defclass result ()
+  (
+   (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+   (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+   (field-name
+      :accessor field-name
+      :initform (make-instance 'field
+                               :display-prefix "Display Prefix"
+                               :constraint '()))
+   );end slot defs
+); end defclass
+
+
+; this object will capture the value, what will be printed, and the data that constrains the value
 (defclass field ()
   ((raw-value
-    :documentation "Value of the field. Normally used directly in data structure definitions (class/struct, etc)"
+    :documentation "value of the field. Normally used directly in data structure definitions (class/struct, etc)"
     :accessor value)
    (constraint
-    :documentation  "declares what the valid values are or what the range is"
+    :documentation  "declares what the valid values are or what the range is, to be used with condition system"
     :initarg :constraint
     :accessor constraint)
    (display-prefix
-    :documentation "Intended to accompany display output"
+    :documentation "intended to accompany display output"
     :initarg :display-prefix
     :accessor display-prefix)))
- 
-   
-
+; TODO find "static" qualifier or extend MOP with :singleton     
